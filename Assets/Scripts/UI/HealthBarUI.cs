2253bad331ac5c474180131d1a3ed9109d475d53
Veera Suominen <veera.suomine@gmail.com>
2023-03-13 11:14:07 +0200

    healthbar

---
+++ Assets/Scripts/UI/HealthBarUI.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.UIElements;
+
+public class HealthBarUI : MonoBehaviour
+{
+    public Slider slider;
+
+    public void SetHealth(int health)
+    {
+    }
+}
